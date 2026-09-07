98221d92521309557d73e7df24a3285bf32739d6
Wouter Samaey <wouter.samaey@storefront.be>
2026-09-07 20:23:33 +0200

    Note yt-dlp flags for Original and English-only Vimeo tracks.

--- downloads/start.sh
+++ downloads/start.sh
@@ -7,3 +7,10 @@ yt-dlp --referer "https://app.musora.com/drumeo" \
 --merge-output-format mkv \
 --batch-file urls.txt \
 --download-archive already_done.txt
+
+
+# Sommige videos hebben geen English [en] track, maar heet die gewoon "Original":
+# yt-dlp --referer "https://app.musora.com/drumeo" --write-all-thumbnails  -N 16 --audio-multistreams -f "bv*+ba[format_id*=Original]+ba[language=nl]" --merge-output-format mkv "https://player.vimeo.com/video/1208247277
+
+# Of sommige hebben geen Nederlands, wel Engels:
+#yt-dlp --referer "https://app.musora.com/drumeo" --write-all-thumbnails  -N 16 --audio-multistreams -f "bv*+ba[language=en]" --merge-output-format mkv "https://player.vimeo.com/video/1109152907"
